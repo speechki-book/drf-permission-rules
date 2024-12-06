148a91f319f20ca2810e891e4ba413e2a010bc96
Bulat Kurbanov <kurbanovbul@gmail.com>
2024-12-06 14:02:40 +0100

    Add allow ssl

--- permission_rules/connect.py
+++ permission_rules/connect.py
@@ -7,10 +7,11 @@ HOST = PERMISSION_RULES_SETTINGS["redis"]["host"]
 PORT = PERMISSION_RULES_SETTINGS["redis"]["port"]
 DB = PERMISSION_RULES_SETTINGS["redis"]["db"]
 PASSWORD = PERMISSION_RULES_SETTINGS["redis"]["password"]
+SSL = PERMISSION_RULES_SETTINGS["redis"].get("ssl", False)
 
 
 def get_redis_connect():
-    return redis.Redis(host=HOST, port=PORT, db=DB, password=PASSWORD)
+    return redis.Redis(host=HOST, port=PORT, db=DB, password=PASSWORD, ssl=SSL)
 
 
 __all__ = ["get_redis_connect"]
